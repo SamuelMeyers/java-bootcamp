6a964282ac88b3fdfa6903c6764b39fcb9284087
Sam Meyers <146159527+SamuelMeyers@users.noreply.github.com>
2026-08-31 16:10:07 -0400

    Completed Exercise 5

---
+++ examples/module-01-exercises/exercise-05/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int age = 24;
+        long population = 340000000L;
+        double price = 19.99;
+        boolean enrolled = true;
+        char grade = 'A';
+        String name = "Sam";
+
+        System.out.println("Age: " + age);
+        System.out.println("Population: " + population);
+        System.out.println("Price: $" + price);
+        System.out.println("Enrolled: " + enrolled);
+        System.out.println("Grade: " + grade);
+        System.out.println("Name: " + name);
+    }
+}
