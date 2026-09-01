2182f8058920345e251fffce714b0cf13dc48581
Sam Meyers <146159527+SamuelMeyers@users.noreply.github.com>
2026-09-01 13:29:53 -0400

    Completed module 2 practice

---
+++ examples/module-02-exercises/practice-01/practice.java
@@ -0,0 +1,18 @@
+public class practice {
+
+    public static void main(String[] args){
+        int age = 25;
+        double salary = 75000.50;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        String name = "Alice";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(isJavaFun);
+        System.out.println(grade);
+
+    }
+
+}
