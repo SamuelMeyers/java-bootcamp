a4075aa4827aa4f8968dd5da0e6638e3c6da8210
Sam Meyers <146159527+SamuelMeyers@users.noreply.github.com>
2026-09-01 16:41:15 -0400

    Completed some practice module 2

--- examples/module-02-exercises/practice-11/practice11.java
+++ examples/module-02-exercises/practice-11/practice11.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;  // Built-in package
+import tools.Greeting;     // Our package
+
 public class practice11 {
+
+    public static void main(String[] args) {
+
+        // BUILT-IN PACKAGE EXAMPLE
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+
+
+        // USER-DEFINED PACKAGE EXAMPLE
+        Greeting.sayHello();
+
+
+        scanner.close();
+    }
 }
---
+++ examples/module-02-exercises/practice-11/tools/Greeting.java
@@ -0,0 +1,8 @@
+package tools;
+
+public class Greeting {
+
+    public static void sayHello() {
+        System.out.println("Hello!");
+    }
+}
