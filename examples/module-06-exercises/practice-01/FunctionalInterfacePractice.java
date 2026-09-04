440c9fb322e6fee8c405805c466803c22eba4d6d
Sam Meyers <146159527+SamuelMeyers@users.noreply.github.com>
2026-09-04 16:01:42 -0400

    Completed exercises 1 and 2, added exercise files

--- examples/module-06-exercises/practice-01/FunctionalInterfacePractice.java
+++ examples/module-06-exercises/practice-01/FunctionalInterfacePractice.java
@@ -1,51 +1,93 @@
-import java.util.function.BinaryOperator;
-import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Function;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
-import java.util.function.UnaryOperator;
+import java.util.function.BiPredicate;
 
 public class FunctionalInterfacePractice {
 
     public static void main(String[] args) {
 
-        System.out.println("1. PREDICATE");
+        System.out.println("===== 1. PREDICATE =====");
         predicatePractice();
 
-        System.out.println("\n2. FUNCTION");
+        System.out.println("\n===== 2. FUNCTION =====");
         functionPractice();
 
-        System.out.println("\n3. CONSUMER");
+        System.out.println("\n===== 3. BIFUNCTION =====");
+        biFunctionPractice();
+
+        System.out.println("\n===== 4. CONSUMER =====");
         consumerPractice();
 
-        System.out.println("\n4. SUPPLIER");
+        System.out.println("\n===== 5. SUPPLIER =====");
         supplierPractice();
 
-        System.out.println("\n5. UNARY OPERATOR");
-        unaryOperatorPractice();
-
-        System.out.println("\n6. BINARY OPERATOR");
-        binaryOperatorPractice();
+        System.out.println("\n===== 6. BIPREDICATE =====");
+        biPredicatePractice();
     }
+
+
+    // --------------------------------------------------
+    // 1. Predicate<T>
+    // Abstract method:
+    // boolean test(T t)
+    // --------------------------------------------------
 
     private static void predicatePractice() {
 
         Predicate<Integer> isAdult =
                 age -> age >= 18;
 
-        System.out.println("Age 20: " + isAdult.test(20));
-        System.out.println("Age 15: " + isAdult.test(15));
+        boolean result = isAdult.test(20);
+
+        System.out.println("Is age 20 an adult?");
+        System.out.println(result);
     }
+
+
+    // --------------------------------------------------
+    // 2. Function<T, R>
+    // Abstract method:
+    // R apply(T t)
+    // --------------------------------------------------
 
     private static void functionPractice() {
 
         Function<String, Integer> getLength =
                 text -> text.length();
 
-        int length = getLength.apply("Java");
+        int result = getLength.apply("Java");
 
-        System.out.println("Length of Java: " + length);
+        System.out.println("Length of Java:");
+        System.out.println(result);
     }
+
+
+    // --------------------------------------------------
+    // 3. BiFunction<T, U, R>
+    // Abstract method:
+    // R apply(T t, U u)
+    // --------------------------------------------------
+
+    private static void biFunctionPractice() {
+
+        BiFunction<Integer, Integer, Integer> add =
+                (a, b) -> a + b;
+
+        int result = add.apply(10, 20);
+
+        System.out.println("10 + 20:");
+        System.out.println(result);
+    }
+
+
+    // --------------------------------------------------
+    // 4. Consumer<T>
+    // Abstract method:
+    // void accept(T t)
+    // --------------------------------------------------
 
     private static void consumerPractice() {
 
@@ -55,31 +97,39 @@ public class FunctionalInterfacePractice {
         printName.accept("Samuel");
     }
 
+
+    // --------------------------------------------------
+    // 5. Supplier<T>
+    // Abstract method:
+    // T get()
+    // --------------------------------------------------
+
     private static void supplierPractice() {
 
         Supplier<String> message =
                 () -> "Welcome to Module 6!";
 
-        System.out.println(message.get());
+        String result = message.get();
+
+        System.out.println(result);
     }
 
-    private static void unaryOperatorPractice() {
 
-        UnaryOperator<Integer> doubleNumber =
-                number -> number * 2;
+    // --------------------------------------------------
+    // 6. BiPredicate<T, U>
+    // Abstract method:
+    // boolean test(T t, U u)
+    // --------------------------------------------------
 
-        System.out.println(
-                "10 doubled: " + doubleNumber.apply(10)
-        );
-    }
+    private static void biPredicatePractice() {
 
-    private static void binaryOperatorPractice() {
+        BiPredicate<String, Integer> hasMinimumLength =
+                (text, minimum) -> text.length() >= minimum;
 
-        BinaryOperator<Integer> add =
-                (a, b) -> a + b;
+        boolean result =
+                hasMinimumLength.test("Java", 4);
 
-        System.out.println(
-                "10 + 20 = " + add.apply(10, 20)
-        );
+        System.out.println("Is Java at least 4 characters?");
+        System.out.println(result);
     }
 }
